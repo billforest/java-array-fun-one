469c6e4854542a75b50b7a88c36ac22c3b308628
jaehyung Lim <jaehyunglimsp15@gpa.ac.kr>
2015-10-13 13:57:57 +0900

    array

---
+++ Marbles.java
@@ -0,0 +1,19 @@
+/*
+ * Project: Marbles.java
+ * Description: A String array containing the names of marble colors
+ * Name: Jaehyung Lim
+ * Date: Oct 13, 2015
+ */
+
+public class Marbles {
+  
+  public static void main( String[] args ) {
+    
+    String [] marbleColors = {"red", "blue", "green"};
+    for( int i = 0; i < marbleColors.length; i++) {
+      System.out.println( marbleColors[i] ); 
+    }
+    
+  } // end main()
+  
+} // end class Marbles
